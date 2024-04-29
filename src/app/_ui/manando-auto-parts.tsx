400084bbce98de3a1663ff589941c5add7dbfcf2
ndela-ntu <ntulilindelani4@gmail.com>
2024-04-29 22:01:11 +0200

    added logo

--- src/app/_ui/manando-auto-parts.tsx
+++ src/app/_ui/manando-auto-parts.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function ManandoAutoParts() {
-  return (
-    <div className={`text-4xl text-white bg-blue rounded-md bg-blue-600 py-2`}>
-      Manando Auto Parts
-    </div>
-  );
+  return <Image src="/logo.png" width={150} height={50} alt="Logo of app" />;
 }
+
+/*<div className={`text-4xl text-white bg-blue rounded-md bg-blue-600 py-2`}>
+Manando Auto Parts
+</div>*/
